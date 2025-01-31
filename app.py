35a9dfb5258e3506bd6fbea72f333d48b03e0fc9
stuckonstandby <fortin.stephane1@gmail.com>
2025-01-31 15:29:59 +0000

    small commit

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
+# Import necessary libraries
 import streamlit as st
 import pandas as pd
 import altair as alt
 from datetime import datetime
-from pages import current_client, new_business_on_gas # Import your page files
+from pages import current_client, new_business_on_gas
 
+# Set the title of the app
+st.title("Steph's Simulation Calc")
 
-# Define the homepage
-def app():
-    # Display the title
-    st.title("Steph's Gas Calc")
-    
-    # Add a block of text
-    st.write("""
-        Welcome to Steph's gas cost calculator designed to simulate 
-        market performance for large volume natural gas users in Ontario!
-    """)
+# Display the welcome message
+st.write(
+    "Welcome to Steph's gas cost calculator designed to simulate market performance for large volume energy users in Canada!"
+)
 
-# Run the homepage if this is the main file
+# (Optional) Add navigation or additional content here
+# For example, you can add buttons to navigate to different pages
+# st.sidebar.success("Select a page above.")
+
 if __name__ == "__main__":
     app()
