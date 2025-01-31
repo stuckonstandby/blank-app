8c284b510cb0bbcf106c68d7fbdc78be9a310447
stuckonstandby <fortin.stephane1@gmail.com>
2025-01-31 20:55:20 +0000

    qc 2

--- pages/new_business_qc_gas.py
+++ pages/new_business_qc_gas.py
@@ -30,7 +30,7 @@ def main():
     # 1. Load Historical Rate Data
     # -------------------------------
     st.header("Historical Rate Data")
-    csv_path = "/workspaces/blank-app/market-data/historical_data_AB_ele.csv"
+    csv_path = "/workspaces/blank-app/market-data/historical_data_QC_gas.csv"
     try:
         df = pd.read_csv(csv_path, parse_dates=["date"])
     except FileNotFoundError:
